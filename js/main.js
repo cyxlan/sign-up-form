9369fca9ff9a7fe95faecd933cfde78d3c4b8044
cyxlan <49080055+cyxlan@users.noreply.github.com>
2024-05-01 15:28:35 -0400

    Check for password match

--- js/main.js
+++ js/main.js
@@ -1,8 +1,20 @@
 form = document.querySelector("form");
 submitBtn = document.querySelector('button[type="submit"]');
+password = document.querySelector("#password");
+confirmPassword = document.querySelector("#confirm_password");
+errorMsg = document.querySelector(".error-msg");
 
 submitBtn.addEventListener("click", (e) => {
   e.preventDefault();
   // run default validation
   form.reportValidity();
+
+  // check if passwords match
+  if (password.value !== confirmPassword.value) {
+    password.setCustomValidity("Passwords do not match.");
+    confirmPassword.setCustomValidity("Passwords do not match.");
+    errorMsg.style.visibility = "visible";
+  } else {
+    form.submit();
+  }
 })
